544a4aba7988842094eeb9c0b9aeb07cc299e428
Timmy Chen <chenx343@gmail.com>
2023-08-25 19:52:15 -0400

    implement add, subtract, multiply, and divide functions

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-function inputNumber(e) {
+function displayInputNumber(e) {
   const inputText = document.querySelector(".input-text");
   const expressionTracker = document.querySelector(".expression-tracker");
   inputText.textContent = inputText.textContent + (e.target.textContent);
   expressionTracker.textContent = (e.target.textContent);
 }
 
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1/num2;
+}
 
 function init() {
   const inputBtns = document.querySelectorAll(".input-btn");
 
   inputBtns.forEach((btn) => {
-    btn.addEventListener("click", inputNumber)
+    btn.addEventListener("click", displayInputNumber)
   })
 }
 
