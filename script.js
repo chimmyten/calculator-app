1fb5918583e7f30f10c5ed79df1cc5d5e00b6f05
Timmy Chen <chenx343@gmail.com>
2023-08-25 20:17:47 -0400

    render operation button clicks and fix spacing

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function displayInputNumber(e) {
-  const inputText = document.querySelector(".input-text");
-  const expressionTracker = document.querySelector(".expression-tracker");
-
   const inputNumberBox = document.querySelector(".input-number");
   const expressionNumber = document.querySelector(".expression-tracker-number");
 
   inputNumberBox.textContent += e.target.textContent;
   expressionNumber.textContent += e.target.textContent;
+}
+
+function displayOperation(e) {
+  const inputOperation = document.querySelector(".input-operation");
+  const expressionOperation = document.querySelector(".expression-tracker-operation");
+  inputOperation.textContent = e.target.textContent;
+  expressionOperation.textContent = e.target.textContent;
 }
 
 function add(num1, num2) {
@@ -27,7 +31,7 @@ function divide(num1, num2) {
 
 function init() {
   const numberBtns = document.querySelectorAll(".number-btn");
-  const operationBtns = document.querySelector(".operation-btn");
+  const operationBtns = document.querySelectorAll(".operation-btn");
 
   numberBtns.forEach((btn) => {
     btn.addEventListener("click", displayInputNumber);
