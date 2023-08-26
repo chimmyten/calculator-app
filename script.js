7b3663a72716cb0f57a2be065eec451d6cb38af1
Timmy Chen <chenx343@gmail.com>
2023-08-25 20:11:53 -0400

    change inputs to multiple divs

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function displayInputNumber(e) {
   const inputText = document.querySelector(".input-text");
   const expressionTracker = document.querySelector(".expression-tracker");
-  inputText.textContent = inputText.textContent + (e.target.textContent);
-  expressionTracker.textContent = (e.target.textContent);
+
+  const inputNumberBox = document.querySelector(".input-number");
+  const expressionNumber = document.querySelector(".expression-tracker-number");
+
+  inputNumberBox.textContent += e.target.textContent;
+  expressionNumber.textContent += e.target.textContent;
 }
 
 function add(num1, num2) {
@@ -18,14 +22,19 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-  return num1/num2;
+  return num1 / num2;
 }
 
 function init() {
-  const inputBtns = document.querySelectorAll(".input-btn");
+  const numberBtns = document.querySelectorAll(".number-btn");
+  const operationBtns = document.querySelector(".operation-btn");
 
-  inputBtns.forEach((btn) => {
-    btn.addEventListener("click", displayInputNumber)
+  numberBtns.forEach((btn) => {
+    btn.addEventListener("click", displayInputNumber);
+  });
+
+  operationBtns.forEach((btn) => {
+    btn.addEventListener("click", displayOperation);
   })
 }
 
